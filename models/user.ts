3d8d70d40e557305f74f34452331ba0570c62378
Rahul Yadav <rahul@growthx.club>
2023-11-13 22:18:30 +0530

    feat: user model updated for password

--- models/user.ts
+++ models/user.ts
@@ -5,6 +5,8 @@ const userSchema = new mongoose.Schema(
     first_name: String,
     last_name: String,
     email: String,
+    salt: String,
+    hash: String,
   },
   { timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' } }
 );
