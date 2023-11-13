396bc9f8bd4fecb78aa50e7b76c7592549112843
Rahul Yadav <rahul@growthx.club>
2023-11-13 20:42:10 +0530

    feat: upodate to existing snippet data

--- app/api/code/delete/route.ts
+++ app/api/code/delete/route.ts
@@ -3,36 +3,46 @@ import { Snippet } from '@/models/snippet';
 import { NextResponse } from 'next/server';
 
 export async function GET(req: Request) {
-
-    if(req.method !== 'GET') {
-        
-        return NextResponse.json({success: false, message: `${req.method} method is not allowed`}, {status: 405})
-    }
+  if (req.method !== 'GET') {
+    return NextResponse.json(
+      { success: false, message: `${req.method} method is not allowed` },
+      { status: 405 }
+    );
+  }
   try {
     await mongoConnect();
 
     const token = req.headers.get('Authorization');
 
-    if(token !== `Bearer ${process.env.CRON_SECRET}`) {
-        return NextResponse.json({
-            success: false,
-            message: 'unauthorized'
-        }, {
-            status: 401
-        })
+    if (token !== `Bearer ${process.env.CRON_SECRET!}`) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: 'unauthorized',
+        },
+        {
+          status: 401,
+        }
+      );
     }
 
     const res = await Snippet.deleteMany({ author: { $exists: 0 } });
 
-    return NextResponse.json({
-      success: true,
-      message: 'snippets deleted successfully',
-    }, {status: 200});
+    return NextResponse.json(
+      {
+        success: true,
+        message: 'snippets deleted successfully',
+      },
+      { status: 200 }
+    );
   } catch (err) {
     console.log(err);
 
-    return NextResponse.json({success: false, message: 'internal server error'}, {
-        status: 500
-    })
+    return NextResponse.json(
+      { success: false, message: 'internal server error' },
+      {
+        status: 500,
+      }
+    );
   }
 }
